f93c1cff989e22e3bbbee05cb6596a1ce05f0d12
smaltman <smaltman@ncsu.edu>
2023-02-14 16:11:06 -0500

    worked on home page elements

--- sherpa/src/components/HomePage.jsx
+++ sherpa/src/components/HomePage.jsx
@@ -1,10 +1,25 @@
 import Header from "./base/header"
+import './HomePage.css'
+import skiLft from '../assets/ski-lift.jpeg';
 
 const HomePage = () => {
-    return(
-        <div>
+    return (
+        <div className="body">
             <Header></Header>
-            <h1>Hello World</h1>
+            <div className="skiLift">
+                <img src={skiLft} alt="ski lift"></img>
+            </div>
+            <div className="search">
+
+            </div>
+            <div className="findBy">
+                <div className="state">
+                    <h2>Find By State</h2>
+                </div>
+                <div className="rating">
+                    <h2>Find By Rating</h2>
+                </div>
+            </div>
         </div>
     );
 }
