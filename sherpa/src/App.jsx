aacfb07d1aeddf178652420db85a66b16e28e8c5
smaltman <smaltman@ncsu.edu>
2023-02-22 10:48:08 -0500

    cleaning up main branch

--- sherpa/src/App.jsx
+++ sherpa/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import {Route, Routes} from 'react-router-dom';
 import './App.css'
-import HomePage from './components/HomePage';
-import UserProfile from './components/user/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +8,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/profile" element={<UserProfile/>}></Route>
       </Routes>
     </div>
   )
